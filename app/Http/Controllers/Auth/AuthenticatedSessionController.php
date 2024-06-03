76dbc4681a949d5c96b9c2231c5ff4df1c8dcc26
awodesigns-github <awo.designs1@gmail.com>
2024-06-03 13:42:47 +0300

    Login and logout logs implementation

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -36,18 +37,22 @@ class AuthenticatedSessionController extends Controller
 
         switch ($userRole) {
             case 'admin':
+                Log::info("User with Id: " . Auth::user()->id . " and name: " . Auth::user()->name . " logged in as admin");
                 return redirect()->route('admin-dashboard');
                 break;
 
             case 'instructor':
+                Log::info("User with Id: " . Auth::user()->id . " and name: " . Auth::user()->name . " logged in as instructor");
                 return redirect()->route('instructor-dashboard');
                 break;
 
             case 'student':
+                Log::info("User with Id: " . Auth::user()->id . " and name: " . Auth::user()->name . " logged in as student");
                 return redirect()->route('spcs-dashboard');
                 break;
 
             default:
+                Log::info("User with Id: " . Auth::user()->id . " and name: " . Auth::user()->name . " logged in as student");
                 return redirect()->route('spcs-dashboard');
                 break;
         }
@@ -59,6 +64,8 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
+        Log::info("User with Id: " . Auth::user()->id . " and name: " . Auth::user()->name . " logged out as " . Auth::user()->roles->first()->name);
+
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
